7db28b600db47d17e32a0e5271530adad31ea49d
Atsuro <atsumomotarou@gmail.com>
2025-08-09 13:46:16 +0900

    チャットルームの作成機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,23 +1,36 @@
 class GroupsController < ApplicationController
+  def new
+    @users = User.where.not(id: current_user.id)
+  end
+
   def show
     @group = Group.find(params[:id])
     @post = Post.new
+    @posts = @group.posts.all
   end
 
 
   def create
-    current_user = User.find(params[:current_user_id])
-    user = User.find(params[:user_id])
-    group_name = "#{current_user.name}と#{user.name}のトークルーム"
-    @group= Group.new(name: group_name)
-    if @group.save
-        user_ids = [ params[:current_user_id], params[:user_id] ]
-        user_ids.each do |user_id|
-          GroupUser.create(user_id: user_id, group_id: @group.id)
-        end
-        redirect_to group_path(@group), notice: "グループを作成しました"
+    if params[:user_ids].blank?
+      redirect_to new_group_path, alert: "メンバーを選択してください"
+      return
+    end
+      user_ids = params[:user_ids].map(&:to_i)
+      user_ids << current_user.id
+      user_ids.uniq!
+      group_users = GroupUser.where(user_id: user_ids)
+      grouped =group_users.group_by { |group_user| group_user.group_id }
+      group_id = grouped.find { |group_id, users|users.map(&:user_id).uniq.sort == user_ids.sort }&.first
+    if group_id.present?
+      @group = Group.find(group_id)
+      redirect_to group_path(@group), notice: "グループに入室しました"
     else
-        render :new, status: :unprocessable_entity
+      group_name = user_ids.map { |id| User.find(id).name }.join("、")+"のグループ"
+      @group= Group.create(name: group_name)
+      user_ids.each do |user_id|
+        GroupUser.create(user_id: user_id, group_id: @group.id)
+      end
+        redirect_to group_path(@group), notice: "グループを作成しました"
     end
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+  def create
+    group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.group_id = group.id
+
+    if @post.save
+
+      redirect_to group_path(group)
+
+    else
+      redirect_to group_path(group)
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
